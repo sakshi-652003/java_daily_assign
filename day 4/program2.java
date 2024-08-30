f26d38226ebe2c61cdf08294d519f507334f6e5d
sakshisalunkhe0605@gmail.com <sakshisalunkhe0506@gmail.com>
2024-08-30 21:00:29 +0530

    day 4

---
+++ day 4/program2.java
@@ -0,0 +1,9 @@
+class Demo{
+	public static void main(String...Demo){
+	for(char i='A';i<='I' ; i++){
+	if(i%2==1){
+	System.out.println (i);
+}
+}
+}
+}
---
+++ day 4/program3.java
@@ -0,0 +1,13 @@
+class Codex{
+	public static void main(String...Codex){
+	for(int i=1; i<=3; i++){
+	for(int j=1; j<=4; j++){
+	System.out.print("* ");
+}
+	
+	System.out.println();
+}
+
+
+}
+}
